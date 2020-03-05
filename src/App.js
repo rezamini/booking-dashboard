6c07902e8265666f816c1d7bb5abe58930f1eeac
rezamini <reza.amini.b@gmail.com>
2020-03-05 15:50:42 +0100

    add main component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DashboardNav from './components/dashboardnav/DashboardNav';
+import DashboardMain from './components/dashboardmain/DashboardMain';
 
 function App() {
   return (
     <div className="App">
       <DashboardNav />
+      <DashboardMain />
       <h1>react appp is heere</h1>
     </div>
   );
---
+++ src/components/dashboardmain/DashboardMain.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+function DashboardMain() {
+  return (
+    <div>
+      <input type="date" id="datetime" name="datetime" />
+    </div>
+  );
+}
+
+export default DashboardMain;

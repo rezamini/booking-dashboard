bb55a2b5a06f6b6ecde5dfee7ced0231956239cf
Tanveer Jan <tjan90@yahoo.com>
2020-03-05 16:11:03 +0100

    mdb_installation

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { createStore } from 'redux';
 import allReducer from './reducers';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 const store = createStore(allReducer);
 
